d8207b410e95e0f42a387a6b4a0bfa745ab0b10a
DrMangoTea <adam.nemec.07@seznam.cz>
2023-03-11 20:52:07 +0100

    energy archer ui

--- src/main/java/dev/Cosmos616/technomancy/TechnomancyClient.java
+++ src/main/java/dev/Cosmos616/technomancy/TechnomancyClient.java
@@ -1,15 +1,27 @@
 package dev.Cosmos616.technomancy;
 
+import dev.Cosmos616.technomancy.content.curiosities.weapons.firearms.archer.EnergyArcherItem;
+import dev.Cosmos616.technomancy.content.curiosities.weapons.firearms.archer.ui.EnergyArcherUI;
 import dev.Cosmos616.technomancy.content.curiosities.weapons.firearms.base.FirearmInteractionHandler;
 import dev.Cosmos616.technomancy.content.curiosities.weapons.firearms.base.FirearmRenderHandler;
+import net.minecraftforge.client.gui.ForgeIngameGui;
+import net.minecraftforge.client.gui.OverlayRegistry;
 import net.minecraftforge.eventbus.api.IEventBus;
 
 public class TechnomancyClient {
 	public static final FirearmRenderHandler FIREARM_RENDER_HANDLER = new FirearmRenderHandler();
 	public static final FirearmInteractionHandler FIREARM_INTERACTION_HANDLER = new FirearmInteractionHandler();
-	
+
 	public static void onClient(IEventBus modEventBus, IEventBus forgeEventBus) {
 		FIREARM_RENDER_HANDLER.registerListeners(forgeEventBus);
 		forgeEventBus.register(FIREARM_INTERACTION_HANDLER);
+		registerOverlays();
 	}
+	private static void registerOverlays() {
+		// Register overlays in reverse order
+
+		OverlayRegistry.registerOverlayAbove(ForgeIngameGui.EXPERIENCE_BAR_ELEMENT, "Test Energy Archer UI", EnergyArcherUI.OVERLAY);
+
+	}
+
 }
--- src/main/java/dev/Cosmos616/technomancy/events/ClientEvents.java
+++ src/main/java/dev/Cosmos616/technomancy/events/ClientEvents.java
@@ -2,14 +2,20 @@ package dev.Cosmos616.technomancy.events;
 
 import com.simibubi.create.content.contraptions.components.fan.AirCurrent;
 import com.simibubi.create.content.contraptions.components.structureMovement.interaction.controls.ControlsHandler;
+import com.simibubi.create.content.contraptions.components.structureMovement.interaction.controls.TrainHUD;
 import com.simibubi.create.content.logistics.item.LinkedControllerClientHandler;
 import dev.Cosmos616.technomancy.TechnomancyClient;
+import dev.Cosmos616.technomancy.content.curiosities.weapons.firearms.archer.ui.EnergyArcherUI;
+import dev.Cosmos616.technomancy.registry.TMItems;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import static dev.Cosmos616.technomancy.content.curiosities.weapons.firearms.archer.ui.EnergyArcherUI.toRender;
 
 @Mod.EventBusSubscriber(Dist.CLIENT)
 public class ClientEvents {
@@ -28,6 +34,12 @@ public class ClientEvents {
 		}
 		
 		TechnomancyClient.FIREARM_RENDER_HANDLER.tick();
+
+
+		Minecraft mc = Minecraft.getInstance();
+		if(!mc.player.getItemInHand(InteractionHand.MAIN_HAND).is(TMItems.ENERGY_ARCHER.get()))
+			EnergyArcherUI.toRender=false;
+		EnergyArcherUI.tick();
 	}
 	
 	protected static boolean isGameActive() {
