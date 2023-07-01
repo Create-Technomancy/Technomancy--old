7765b9b1782f85f93d3406436b9753d885228329
DrMangoTea <adam.nemec.07@seznam.cz>
2023-07-01 20:14:45 +0200

    port to 0.5.1 and archer improvements

---
+++ src/main/java/dev/Cosmos616/technomancy/content/curiosities/weapons/firearms/repeater/EnergyRepeaterModel.java
@@ -0,0 +1,15 @@
+/*
+package dev.Cosmos616.technomancy.content.curiosities.weapons.firearms.repeater;
+
+import com.simibubi.create.foundation.item.render.CustomRenderedItemModel;
+import net.minecraft.client.resources.model.BakedModel;
+
+public class EnergyRepeaterModel extends CustomRenderedItemModel {
+	public EnergyRepeaterModel(BakedModel template) {
+		super(template, "energy_repeater");
+		addPartials("cog");
+		addPartials("rails");
+	}
+}
+
+ */
---
+++ src/main/java/dev/Cosmos616/technomancy/content/curiosities/weapons/firearms/revolver/EnergyRevolverModel.java
@@ -0,0 +1,14 @@
+/*
+package dev.Cosmos616.technomancy.content.curiosities.weapons.firearms.revolver;
+
+import com.simibubi.create.foundation.item.render.CustomRenderedItemModel;
+import net.minecraft.client.resources.model.BakedModel;
+
+public class EnergyRevolverModel extends CustomRenderedItemModel {
+    public EnergyRevolverModel(BakedModel template) {
+        super(template, "energy_revolver");
+        addPartials("cog");
+    }
+}
+
+ */
